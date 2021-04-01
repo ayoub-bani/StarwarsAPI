620d356b5ca9d70d44d5959e64fc58e2953409da
ayoub-bani <ayoub.bani101@gmail.com>
2021-04-01 16:27:54 +0100

    added axios librarie

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,22 @@
 <template>
   <div class="hello">
     <div class="container">
-      <p class="bg-primary text-white">this is a test</p>
+      <p class="bg-primary text-white">Star Wars API</p>
+    </div>
+    <div class="container">
+      <div class="d-flex">
+        <div class="card" style="width: 18rem;">
+          <img src="#" class="card-img-top" alt="#">
+          <div class="card-body">
+            <h5 class="card-title">{{ CharacterData.name }}</h5>
+            <p class="card-text">
+              <span>height:</span><span>{{ CharacterData.height }}</span>
+              <span>mass:</span><span>{{ CharacterData.mass }}</span>
+              <span>hair color:</span><span>{{ CharacterData.hair_color }}</span>
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -10,7 +25,23 @@
   export default {
     name: 'HelloWorld',
     props: {
-      msg: String
+      msg: String,
+      id: String,
+      data: function () {
+        return {
+          CharacterData: {}
+        }
+      }
+    },
+    methods: {
+      getUnits: function () {
+        fetch(`https://swapi.dev/api/people/${this.id}/`)
+          .then(response => response.json())
+          .then(data => this.CharacterData = data)
+      }
+    },
+    created: function () {
+      this.getUnits()
     }
   }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import Vue from 'vue'
 import App from './App.vue'
 
 Vue.config.productionTip = false
